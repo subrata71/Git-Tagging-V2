8a41e7cfcc1c8a8c5cc2c56f657642e0642845d3
subrata <subrata@appsmith.com>
2025-03-11 14:01:51 +0600

    Add semicolon back again

--- modules/JSModule1/jsobjects/JSModule1/JSModule1.js
+++ modules/JSModule1/jsobjects/JSModule1/JSModule1.js
@@ -4,7 +4,7 @@ export default {
 	myFun1 () {
 		//	write code here
 		//	this.myVar1 = [1,2,3]
-		return GetUsers.run()
+		return GetUsers.run();
 	},
 	async myFun2 () {
 		//	use async-await or promises
